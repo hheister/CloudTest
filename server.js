57ecd25097edd08f1b22b51bdbae2f9b1c3e1c45
hheister <horst.heistermann@oracle.com>
2015-06-01 13:04:55 -0700

    Updated mongoose connection string for free mongodb

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
 app = config(app);
 
-mongoose.connect('mongodb://horst:heidi@candidate.45.mongolayer.com:10545/app37413426');
+mongoose.connect('mongodb://heroku_app37413426:c2qshsi9d8lrv0p9usgod8amom@ds043022.mongolab.com:43022/heroku_app37413426');
 mongoose.connection.on('open', function() {
     console.log('Mongoose connected.');
 });
