e11ce4fc00e2559acf5ad9a5f5ce7371d53ea24c
hheister <horst.heistermann@oracle.com>
2015-06-01 12:50:03 -0700

    Update  mongoose connection string

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
 app = config(app);
 
-mongoose.connect('mongodb://localhost/imgPloadr');
+mongoose.connect('mongodb://horst:heidi@candidate.45.mongolayer.com:10545/app37413426');
 mongoose.connection.on('open', function() {
     console.log('Mongoose connected.');
 });
